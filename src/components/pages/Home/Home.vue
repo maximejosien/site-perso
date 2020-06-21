07511f726c08173010ba97080981395403bc2972
Maxime Josien <maxime@disruptual.com>
2020-06-21 14:24:37 +0200

    first commit

---
+++ src/components/pages/Home/Home.vue
@@ -0,0 +1,12 @@
+<template src="./Home.html"></template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<style src="./Home.css"></style>
